ec017cdf480380de6d045ce3a57e65832114ede0
AbdallahAlameer <abdallahalameer652@gmail.com>
2025-07-09 17:48:45 +0300

    Create altERnaTIng cAsE <=> ALTerNAtiNG CaSe.cpp
    Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase

---
+++ 8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe.cpp
@@ -0,0 +1,20 @@
+std::string to_alternating_case(const std::string& str)
+{
+  std::string result = "";
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (isupper(str[i]))
+    {
+      result += tolower(str[i]);
+    }
+    else if (islower(str[i]))
+    {
+      result += toupper(str[i]);
+    }
+    else
+    {
+      result += str[i];
+    }
+  }
+	return result;
+}
