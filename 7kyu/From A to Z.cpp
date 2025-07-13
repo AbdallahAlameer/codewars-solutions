80f5143f74d0368af03c8e6bca70291688352fa2
AbdallahAlameer <abdallahalameer652@gmail.com>
2025-07-13 16:07:46 +0300

    Create From A to Z.cpp
    Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
    Note that if the range is given in capital letters, return the string in capitals also!

---
+++ 7kyu/From A to Z.cpp
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+
+string gimme_the_letters(const string& sp)
+{
+  string result = "" ; 
+  
+  for(int i = sp[0]; i <=sp[2]; i++)
+  {
+    result.push_back(char(i));
+  }
+    return result;
+}
