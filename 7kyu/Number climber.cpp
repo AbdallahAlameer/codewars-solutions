d602a872f68f2ba1b308608e968f77121c06ae73
AbdallahAlameer <abdallahalameer652@gmail.com>
2025-07-14 15:16:46 +0300

    Create Number climber.cpp
    For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either the double of the preceeding number or the double plus 1.
    
    For example, given N = 13, the sequence is [1, 3, 6, 13]

---
+++ 7kyu/Number climber.cpp
@@ -0,0 +1,12 @@
+using namespace std ; 
+vector<int> climb(int n)
+{
+  vector<int> result ;
+  while( n > 1 )
+  {
+    result.insert(result.begin(),n) ; 
+    n =  n / 2 ;
+  }
+  result.insert(result.begin(),n) ; 
+    return result;
+}
