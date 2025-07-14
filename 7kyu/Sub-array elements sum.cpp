f99174685dc4baed96a39167c28f662ee59c1456
AbdallahAlameer <abdallahalameer652@gmail.com>
2025-07-14 15:07:19 +0300

    Create Sub-array elements sum.cpp
    Given an array (or list or vector) of arrays (or, guess what, lists or vectors) of integers, your goal is to return the sum of a specific set of numbers, starting with elements whose position is equal to the main array length and going down by one at each step.
    
    Say for example the parent array (etc, etc) has 3 sub-arrays inside: you should consider the third element of the first sub-array, the second of the second array and the first element in the third one: [[3, 2, 1, 0], [4, 6, 5, 3, 2], [9, 8, 7, 4]] would have you considering 1 for [3, 2, 1, 0], 6 for [4, 6, 5, 3, 2] and 9 for [9, 8, 7, 4], which sums up to 16.

---
+++ 7kyu/Sub-array elements sum.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std ;
+long elementsSum(const vector<vector<int>>& arr, int d = 0){
+  //your code here
+  int result = 0 ; 
+  int arr_count = arr.size() ; 
+  for (int i = 0 ; i < arr_count ; i++)
+  {
+    int index_and_count = arr_count - 1 - i ;
+    
+    if( arr[i].size() > index_and_count )
+    {
+      result+= arr[i][ index_and_count ] ; 
+    }
+    else
+      result += d ;
+  }
+  return result ;
+}
